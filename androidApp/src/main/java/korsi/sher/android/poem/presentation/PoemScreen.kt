6fa72c44dc2ea476a4978fb7ed8606b0a35ffc6b
Hamidreza <hamidrezasahraei@cafebazaar.ir>
2023-06-30 01:12:33 +0200

    Remove unused imports from ProgressButton.kt and PoemScreen.kt

--- androidApp/src/main/java/korsi/sher/android/poem/presentation/PoemScreen.kt
+++ androidApp/src/main/java/korsi/sher/android/poem/presentation/PoemScreen.kt
@@ -1,6 +1,5 @@
 package korsi.sher.android.poem.presentation
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,15 +7,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
